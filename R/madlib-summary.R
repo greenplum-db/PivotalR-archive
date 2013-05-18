8b4c708694b7393a6ccf2d07259f95b901092fbe
Qian, Hai <hqian@gopivotal.com>
2013-05-18 13:19:20 -0700

    Continue coding madlib.summary free function
    The only left work: create query string and execute and format the
    results.

--- R/madlib-summary.R
+++ R/madlib-summary.R
@@ -8,5 +8,36 @@ madlib.summary <- function (x, target.cols = NULL, grouping.cols = NULL,
                             ntile = NULL, n.mfv = 10, estimate = TRUE,
                             interactive = TRUE)
 {
+    if (!is(x, "db.obj"))
+        stop("Cannot operate on non db.obj objects!")
+
+    if ((!is.null(target.cols) && !all(target.cols %in% names(x))) ||
+        (!is.null(grouping.cols) && !all(grouping.cols %in% names(x))))
+        stop("target.cols or grouping.cols has columns that are not in ",
+             "the data!")
+    
+    if (is(x, "db.view") || is(x, "db.Rquery")) {
+        if (interactive) {
+            cat(deparse(substitute(x)),
+                "does not point to a table in the database",
+                dbname(conn.id(x)),
+                "and it might take time to create a table and get the summary for this!\n")
+            go <- .read.input("Do you really want to continue ? (Yes/No) : ",
+                              c("yes", "y", "no", "n"))
+            if (go == "no" || go == "n") return
+        }
+        tbl <- .unique.string()
+        if (is(x, "db.Rquery"))
+            tmp <- as.db.data.frame(x, tbl, conn.id(x), FALSE
+                                    is.temp = TRUE,
+                                    verbose = interactive,
+                                    pivot.factor = FALSE)
+        else
+            .db.getQuery(paste("create temp table", tbl,
+                               "as select * from", content(x)), conn.id(x))
+        
+    } else
+        tbl <- content(x)
+
     
 }
