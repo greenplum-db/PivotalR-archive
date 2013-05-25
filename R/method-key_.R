0e11b8c006e171fa8084193079cb4e1b7a70a77c
Qian, Hai <hqian@gopivotal.com>
2013-05-25 00:04:27 -0700

    Added key functions
    Make it easier to change the key

---
+++ R/method-key_.R
@@ -0,0 +1,29 @@
+
+## ------------------------------------------------------------------------
+## get or set key
+## ------------------------------------------------------------------------
+
+key <- function (x)
+{
+    if (! is(x, "db.obj"))
+        stop("The argument must be a db.obj-class object!")
+    x@.key   
+}
+
+## ------------------------------------------------------------------------
+
+"key<-" <- function (x, value = character(0))
+{
+    if (! is(x, "db.obj"))
+        stop("The argument must be a db.obj-class object!")
+
+    if (!is.character(value))
+        stop("key must be a column name, which is a string!")
+
+    if (!value %in% names(x))
+        stop(deparse(substitute(x)), " does not have a column named ",
+             value)
+    
+    x@.key <- value
+    x
+}
