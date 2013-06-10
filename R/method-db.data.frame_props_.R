bb394316fdc278c17010a77c3871ed64adadec0e
Qian, Hai <hqian@gopivotal.com>
2013-06-09 22:56:52 -0700

    Added names<-

--- R/method-db.data.frame_props_.R
+++ R/method-db.data.frame_props_.R
@@ -43,6 +43,56 @@ setMethod (
 
 ## ------------------------------------------------------------------------
 
+setMethod (
+    "names<-",
+    signature(x = "db.obj"),
+    function (x, value) {
+        if (!is.character(value))
+            stop("The value to replace names is not correct!")
+        l1 <- length(names(x))
+        l2 <- length(value)
+        if (l1 < l2) l <- l1
+        else l <- l2
+        if (l == 0)
+            stop("The length of value is not correct!")
+        select <- seq_len(l)
+            
+        if (is(x, "db.data.frame")) {
+            expr <- x@.col.name[select]
+            where <- ""
+            where.str <- ""
+            parent <- content(x)
+            src <- parent
+        } else {
+            expr <- x@.expr[select]
+            where <- x@.where
+            if (where != "") where.str <- paste(" where", x@.where)
+            parent <- x@.parent
+            src <- x@.source
+        }
+        sort <- .generate.sort(x)
+        sql <- paste("select ", paste(expr, value[select], sep = " as ",
+                                      collapse = ", "),
+                     " from ", parent, where.str, sort$str, sep = "")
+        new("db.Rquery",
+            .content = sql,
+            .expr = expr,
+            .source = src,
+            .parent = parent,
+            .conn.id = conn.id(x),
+            .col.name = value[select],
+            .key = key(x),
+            .col.data_type = x@.col.data_type[select],
+            .col.udt_name = x@.col.udt_name[select],
+            .where = where,
+            .is.factor = x@.is.factor[select],
+            .factor.suffix = x@.factor.suffix[select],
+            .sort = sort)
+    },
+    valueClass = "db.Rquery")
+
+## ------------------------------------------------------------------------
+
 content <- function (x)
 {
     if (! is(x, "db.obj"))
