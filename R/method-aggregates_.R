7eaaae662eccc9131a795a657b989cd4dacd09ee
Qian, Hai <hqian@gopivotal.com>
2013-06-17 23:28:12 -0700

    Added colAgg and rowAgg to replace c or cagg

--- R/method-aggregates_.R
+++ R/method-aggregates_.R
@@ -222,14 +222,113 @@ setMethod (
 
 ## ------------------------------------------------------------------------
 
-setGeneric ("cagg", signature = "x", def = function(x) standardGeneric("cagg"))
+## array_agg each column
+colAgg <- function (x)
+{
+    if (!is(x, "db.obj"))
+        stop("this function only works with db.obj!")
+    .aggregate(x, "array_agg", FALSE, NULL, FALSE, "ARRAY",
+               paste("_", x@.col.udt_name, sep = ""))
+}
 
-setMethod (
-    "cagg",
-    signature(x = "db.obj"),
-    function (x) {
-        .aggregate(x, "array_agg", TRUE, NULL, FALSE, "ARRAY",
-                   paste("_", x@.col.udt_name, sep = ""))
-    },
-    valueClass = "db.Rquery")
+## ------------------------------------------------------------------------
 
+## array_agg all the columns on the same row
+rowAgg <- function (x, ...)
+{
+    n <- nargs()
+    if (!is(x, "db.obj")) stop("this function only works with db.obj!")
+    if (!all(x@.col.data_type %in% .num.types) ||
+        !all(x@.col.data_type %in% .txt.types) ||
+        !all(x@.col.data_type %in% c('boolean')))
+        stop("columns cannot be put into the same array!")
+    for (i in seq_len(n-1)) {
+        y <- eval(parse(text = paste0("..", i)))
+        if (!is(y, "db.obj")) stop("this function only works with db.obj!")
+        if (!.eql.parent(x, y))
+            stop("all the objects must originate from the same source!")
+        data.types <- c(x@.col.data_type[1], y@.col.data_type)
+        if (!all(data.types %in% .num.types) ||
+            !all(data.types %in% .txt.types) ||
+            !all(data.types %in% c('boolean')))
+            stop("columns cannot be put into the same array!")
+    }
+
+    if (x@.col.data_type %in% .num.types)
+        udt.name <- "_float8"
+    else if (x@.col.data_type %in% .txt.types)
+        udt.name <- "_text"
+    else
+        udt.name <- "_bool"
+    
+    if (is(x, "db.data.frame")) {
+        tbl <- content(x)
+        src <- tbl
+        parent <- tbl
+    } else {
+        if (x@.parent != x@.source)
+            tbl <- paste("(", x@.parent, ") s", sep = "")
+        else
+            tbl <- x@.parent
+        parent <- x@.parent
+        src <- x@.source
+    }
+
+    if (is(x, "db.Rquery") && x@.where != "") {
+        where.str <- paste(" where", x@.where)
+        where <- x@.where
+    } else {
+        where.str <- ""
+        where <- ""
+    }
+
+    sort <- .generate.sort(x)
+
+    expr <- "array["
+    if (is(x, "db.data.frame"))
+        expr <- paste(expr, paste(names(x), collapse = ", "), sep = "")
+    else
+        expr <- paste(expr, paste(x@.expr, collapse = ", "), sep = "")
+    for (i in seq_len(n-1)) {
+        y <- eval(parse(text = paste0("..", i)))
+        if (is(y, "db.data.frame"))
+            expr <- paste(expr, paste(names(y), collapse = ", "), sep = "")
+        else
+            expr <- paste(expr, paste(y@.expr, collapse = ", "), sep = "")
+    }
+    expr <- paste(expr, "]", sep = "")
+
+    new("db.Rquery",
+        .content = paste("select ", expr, " as agg_opr from ",
+        tbl, where.str, sort$str, sep = ""),
+        .expr = expr,
+        .source = src,
+        .parent = parent,
+        .conn.id = conn.id(x),
+        .col.name = "agg_opr",
+        .key = character(0),
+        .where = "",
+        .col.data_type = "array",
+        .col.udt_name = udt.name,
+        .is.factor = FALSE,
+        .factor.suffix = "",
+        .sort = sort)
+}
+
+## ------------------------------------------------------------------------
+
+.eql.parent <- function (x1, x2)
+{
+    if (is(x1, "db.data.frame") && is(x2, "db.data.frame")) {
+        return (content(x1) == content(x2) &&
+                conn.eql(conn.id(x1), conn.id(x2)))
+    } else if (is(x1, "db.data.frame") && is(x2, "db.Rquery")) {
+        return (content(x1) == x2@.parent && x2@.where = "" &&
+                conn.eql(conn.id(x1), conn.id(x2)))
+    } else if (is(x1, "db.Rquery") && is(x2, "db.data.frame"))
+        .eql.parent(x2, x1)
+    else {
+        return (x1@.parent == x2@.parent && x1@.where = x2@.where &&
+                conn.eql(conn.id(x1), conn.id(x2)))
+    }
+}
