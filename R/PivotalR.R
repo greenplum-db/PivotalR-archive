a8b6548e79a62a20589f94a19c1028238a542611
Qian, Hai <hqian@gopivotal.com>
2013-07-18 23:46:06 -0700

    Normal message & install shiny when running PivotalR() only

--- R/PivotalR.R
+++ R/PivotalR.R
@@ -5,12 +5,27 @@
 
 PivotalR <- function ()
 {
+    if (!("shiny" %in% .localVars$installed.pkgs)) {
+        message(paste("Package shiny",
+                      " is going to be installed so that ",
+                      .this.pkg.name,
+                      " could connect to databases.\n\n", sep = ""))
+        install.packages(pkgs = "shiny")
+    }
+    
     ## PivotalR's installation path
     .localVars$pkg.path <- path.package(.this.pkg.name)
     ## gui folder installation path
     gui.dir <- paste(.localVars$pkg.path, "/gui", sep = "")
 
-    message("\nWelcome to poor men's Alpine. It is free ...\n")
+    message("\nWelcome to PivotalR GUI. It is free ...\n")
     message("Press Ctrl + c to stop.")
     runApp(gui.dir)
+}
+
+## ------------------------------------------------------------------------
+
+pivotalr <- function ()
+{
+    PivotalR()
 }
