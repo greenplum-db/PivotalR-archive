59947d17f4e875f4c02e9916c086a23b1571462e
Hong Ooi <hooi@gopivotal.com>
2013-09-26 19:44:49 +1000

    S3 methods
    as.data.frame, extractAIC, logLik

---
+++ R/as.data.frame.R
@@ -0,0 +1,8 @@
+as.data.frame.db.table <- as.data.frame.db.view <- as.data.frame.db.Rquery <-
+function(x, row.names=NULL, optional=FALSE, stringsAsFactors=default.stringsAsFactors(), array=TRUE, ...)
+{
+    df <- lookat(x, nrows=NULL, array=array)
+    if(stringsAsFactors && any(sapply(df, "class") == "character"))
+        df[] <- lapply(df, function(x) if(is.character(x)) factor(x) else x)
+    df
+}
