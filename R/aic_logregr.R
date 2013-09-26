483e76bfac473622e9e32c2a8f5010d6b3d306f6
Qian, Hai <hqian@gopivotal.com>
2013-09-26 10:56:22 -0700

    Minor changes

--- R/aic_logregr.R
+++ R/aic_logregr.R
@@ -1,8 +1,10 @@
 extractAIC.logregr.madlib <- function(fit, scale=0, k=2, ...)
 {
     edf <- length(fit$coef)
-    c(edf, -2 * fit$log_likelihood + 2 * edf)
+    c(edf, -2 * fit$log_likelihood + 2 * edf + (k-2) * edf)
 }
+
+## ----------------------------------------------------------------------
 
 logLik.logregr.madlib <- function(object, ...)
 {
@@ -12,11 +14,17 @@ logLik.logregr.madlib <- function(object, ...)
     ll
 }
 
+## ----------------------------------------------------------------------
+
 extractAIC.logregr.madlib.grps <- function(fit, scale=0, k=2, ...)
 lapply(extractAIC, fit, scale=scale, k=2)
 
+## ----------------------------------------------------------------------
+
 logLik.logregr.madlib.grps <- function(object, ...)
 lapply(logLik, object)
+
+## ----------------------------------------------------------------------
 
 AIC.logregr.madlib.grps <- function(object, ..., k=2)
 sapply(AIC, object, k=k)
