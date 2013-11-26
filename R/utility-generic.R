92a5680ac172519ccc322436f377d526cb16d91a
Qian, Hai <hqian@gopivotal.com>
2013-11-26 00:17:46 -0800

    Greatly improved the performance of .unique.string
    The previous .unique.string was so slow that it made many operations
    slower that they should have been.

--- R/utility-generic.R
+++ R/utility-generic.R
@@ -170,7 +170,8 @@ arraydb.to.arrayr <- function (str, type = "double", n = 1)
 
 .unique.string <- function ()
 {
-    r1 <- sample(seq_len(100000000), 1)
+    ## r1 <- sample(seq_len(100000000), 1)
+    r1 <- as.integer(runif(1) * 1e8) + 1
     r2 <- unclass(as.POSIXct(strptime(date(),"%c")))[1]
     r3 <- r2 %% r1
     paste("__madlib_temp_", r1, "_", r2, "_", r3, "__", sep = "")
