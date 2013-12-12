08488a1264051635f3a493bd13420e3d2bf9f547
Qian, Hai <hqian@gopivotal.com>
2013-12-12 13:06:28 -0800

    Started coding

---
+++ R/margins.R
@@ -0,0 +1,72 @@
+## Implement marginal effects
+
+.margins <- function(coef, data, P, var = NULL, at.mean = FALSE)
+{
+    coefs <- coef
+    n <- length(coefs)
+    names(coefs) <- paste("b", seq_len(n), sep = "")
+    if (at.mean) {
+        avgs <- lk(mean(data))
+        names(avgs) <- gsub("_avg$", "", names(avgs))
+        mar <- sapply(var,
+                      function(i) {
+                          eval(parse(text = "with(avgs," %+%
+                                     derv(P, i, coefs) %+% ")"))
+                      })
+        se <- array(0, dim = c(n,n))
+        for (i in seq_len(n))
+            se[i,] <- sapply(seq_len(n),
+                             function(j) {
+                                 eval(parse(text = "with(avgs," %+%
+                                            derv1(P, var[i], j, coefs)
+                                            %+% ")"))
+                             })
+    } else {
+        mar <- sapply(var,
+                      function(i) {
+                          eval(parse(text = "mean(with(data," %+%
+                                     derv(P, i, coefs) %+% "))"))
+                      })
+        mar <- unlist(lk(.combine.list(mar), -1))
+        for (i in seq_len(n)) {
+            if (i == 1)
+                se <- sapply(seq_len(n),
+                             function(j) {
+                                 eval(parse(text = "mean(with(data," %+%
+                                            derv1(P, var[i], j, coefs)
+                                            %+% "))"))
+                             })
+            else
+                se <- c(se, sapply(seq_len(n),
+                                   function(j) {
+                                       eval(parse(text = "mean(with(data," %+%
+                                                  derv1(P, var[i], j, coefs)
+                                                  %+% "))"))
+                                   }))
+        }
+        se <- t(array(unlist(lk(.combine.list(se), -1)), dim = c(n,n)))
+    }
+    return(list(mar=mar, se=se))
+}
+
+## ----------------------------------------------------------------------
+
+## derivative over a variable
+derv <- function(P, x, coefs)
+{
+    s <- .parse.deriv(P, x)
+    w <- eval(parse(text = paste("substitute(", s, ", coefs)", sep = "")))
+    as.character(enquote(w))[2]
+}
+
+## ----------------------------------------------------------------------
+
+## derivative over a variable and a coefficient
+derv1 <- function(P, x, j, coefs)
+{
+    s <- .parse.deriv(P, x)
+    s1 <- .parse.deriv(s, "b" %+% "j")
+    w <- eval(parse(text = paste("substitute(", s1, ", coefs)", sep = "")))
+    as.character(enquote(w))[2]
+}
+
