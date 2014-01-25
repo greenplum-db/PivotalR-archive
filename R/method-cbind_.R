2ea2fc5092e6844d96e13f4e5ee397977f199d48
Qian, Hai <hqian@gopivotal.com>
2014-01-25 09:04:38 -0800

    Minor changes; added sys.call

--- R/method-cbind_.R
+++ R/method-cbind_.R
@@ -21,19 +21,32 @@ setMethod (
 
 ## ----------------------------------------------------------------------
 
-setGeneric("cbind", function(`__madlib_temp_27836b51_49d3_9d9a96_dc459ab0ef62__`, ..., deparse.level = 1) {
-    if (missing(`__madlib_temp_27836b51_49d3_9d9a96_dc459ab0ef62__`) && !is(..1, "db.obj")
-        || (!missing(`__madlib_temp_27836b51_49d3_9d9a96_dc459ab0ef62__`) && !is(`__madlib_temp_27836b51_49d3_9d9a96_dc459ab0ef62__`, "db.obj"))) {
-        if (missing(`__madlib_temp_27836b51_49d3_9d9a96_dc459ab0ef62__`))
+setGeneric(
+    "cbind",
+    function(`__madlib_temp_27836b51_49d3_9d9a96_dc459ab0ef62__`,
+             ..., deparse.level = 1)
+{
+    no.first <- missing(`__madlib_temp_27836b51_49d3_9d9a96_dc459ab0ef62__`)
+    if (no.first && !is(..1, "db.obj") ||
+        (!no.first &&
+         !is(`__madlib_temp_27836b51_49d3_9d9a96_dc459ab0ef62__`,
+             "db.obj")))
+    {
+        if (no.first)
             args <- list(..., deparse.level = deparse.level)
-        else
-            args <- list(`__madlib_temp_27836b51_49d3_9d9a96_dc459ab0ef62__`, ..., deparse.level = deparse.level)
+        else {
+            call <- paste(deparse(sys.call()), collapse = "")
+            args <- list(
+                `__madlib_temp_27836b51_49d3_9d9a96_dc459ab0ef62__`, ...,
+                deparse.level = deparse.level)
+        }
         do.call(base::cbind, args)
     } else {
-        if (missing(`__madlib_temp_27836b51_49d3_9d9a96_dc459ab0ef62__`))
+        if (no.first)
             lst <- c(...)
         else
-            lst <- c(`__madlib_temp_27836b51_49d3_9d9a96_dc459ab0ef62__`, ...)
+            lst <- c(`__madlib_temp_27836b51_49d3_9d9a96_dc459ab0ef62__`,
+                     ...)
         .combine.list(lst)
     }
 })
