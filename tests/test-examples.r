5741a96ac5e7403f208ea937e81ab4ca56108a9b
Qian, Hai <hqian@gopivotal.com>
2014-02-05 23:42:05 -0800

    Added more example tests
    Need to:
    * help messages for testing function

--- tests/test-examples.r
+++ tests/test-examples.r
@@ -20,13 +20,22 @@ dat.mm <- abalone
 ## ----------------------------------------------------------------------
 ## Tests
 
-test_that("madlib.lm returns the correct class", {
+test_that("madlib.lm is fast enough", {
+    expect_that(madlib.lm(rings ~ . - id - sex, data = dat.db), takes_less_than(2))
+})
+
+test_that("madlib.lm returns the correct values", {
     ##
     fdb <- madlib.lm(rings ~ . - id - sex, data = dat.db)
-    fm <- lm(rings ~ . - id - sex, data = dat.mm)
+    fm <- summary(lm(rings ~ . - id - sex, data = dat.mm))
     ##
-    expect_that(fdb, is_a("lm.madlib"))
-    expect_that(fdb$coef, equals(fm$coefficients))
+    expect_that(fdb,                    is_a("lm.madlib"))
+    expect_that(fdb$data,               is_a("db.data.frame"))
+    expect_that(fdb$coef,               equals(as.numeric(fm$coefficients[,1])))
+    expect_that(fdb$std_err,            is_equivalent_to(fm$coefficients[,2]))
+    expect_that("id" %in% fdb$col.name, is_false())
+    expect_that(fdb$has.intercept,      is_true())
+    expect_that(fdb$r2,                 is_identical_to(fm$r.squared))
 })
 
 ## ----------------------------------------------------------------------
