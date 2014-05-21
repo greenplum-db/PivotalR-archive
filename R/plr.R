cdbdf7c34a280019880c71b927aea64c46b7d29c
Qian, Hai <hqian@gopivotal.com>
2014-05-20 17:14:20 -0700

    Exported declare

--- R/plr.R
+++ R/plr.R
@@ -59,37 +59,6 @@ declare <- function(...) return (invisible(..1))
 
 ## ------------------------------------------------------------
 
-## ## Map function
-## plr.chunkmap <- function(grp.data, INDICES, FUN, ...)
-## {
-##     parse <- .plr.parser(FUN)
-##     fun.body <- parse$fun.str
-##     fun.types <- parse$types
-##
-##     ## function arguments and return types
-##     arg.types <- fun.types[Filter(function(s) s != "", names(fun.types))]
-##     ret.type <- fun.types[which(names(fun.types) == "")[1]][[1]]
-##
-##     ## grouped intermediate table
-##     if (is.null(attr(data, "grouped.data")))
-##         grp.data <- as.db.data.frame(by(data[,names(arg.types)], INDICES, colAgg))
-##     else
-##         ## Already done that, no need to do it any more
-##         grp.data <- attr(data, "grouped.data")
-##
-##     ## Create return compisite type
-##     db.rettype <- .create_plr_rettype(ret.type) # name of return type in database
-##
-##     ## Create a temporary PL/R function
-##     plr.func <- .plr(fun.body, arg.types, db.rettype)
-##
-##     ## Execute the PL/R function and create the result table
-##     models <- as.db.data.frame(plr.func(grp.data), .unique.string())
-##     models
-## }
-
-## ------------------------------------------------------------
-
 ## Reduce function
 plr.reduce <- function(data, FUN, ...)
 {
