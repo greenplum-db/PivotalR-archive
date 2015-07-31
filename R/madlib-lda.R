7a7b64ff154d62945a807fb69d83b990e3499ade
Sam Ziegler <sziegler@pivotal.io>
2015-07-31 12:24:26 -0700

    finished preliminary version of madlib-lda.R training function. Also fixed typo in random forest.

--- R/madlib-lda.R
+++ R/madlib-lda.R
@@ -3,7 +3,7 @@
 setClass("lda.madlib")
 
 madlib.lda <- function (data, docid, words, topic_num,
-                        alpha, beta, iter_num = 20,
+                        alpha, eta, iter_num = 20,
                        ...) {
     if ( ! is( data, "db.obj" ) )
         stop( "madlib.lda can only be used on a db.obj object, ",
@@ -47,10 +47,6 @@ madlib.lda <- function (data, docid, words, topic_num,
 
     #vocabulary <- arraydb.to.arrayr(vocabulary, "character", voc_size)
 
-    print(voc_size)
-    print(class(vocabulary))
-    print(dim(vocabulary))
-
     #print(voc_size)
 
 
@@ -59,7 +55,7 @@ madlib.lda <- function (data, docid, words, topic_num,
                  tbl.tf, "', '", tbl.model, "', '",
                  tbl.output, "',", voc_size, ", ",
                  topic_num, ",", iter_num, ",", alpha, 
-                 ",", beta, ")", sep = "")
+                 ",", eta, ")", sep = "")
 
     #print(sql)
     
@@ -91,8 +87,28 @@ madlib.lda <- function (data, docid, words, topic_num,
     # print(topics)
     # print(topic_sums)
 
-    res_out
+    document_sums <- arraydb.to.arrayr(res_out$topic_count)
+    document_sums <- t(document_sums)
 
+    assignments <- res_out$topic_assignment
+    assignments <- lapply(assignments, arraydb.to.arrayr)
+
+    #####
+
+    rst <- list()
+
+    rst$assignments <- assignments
+    rst$topics <- topics
+    rst$topic_sums <- topic_sums
+    rst$document_sums <- document_sums
+
+    class(rst) <- "lda.madlib"
+
+    .restore.warnings(warnings)
+
+    return (rst)
+
+    
 
    
     
@@ -102,21 +118,9 @@ madlib.lda <- function (data, docid, words, topic_num,
     #model <- db.data.frame(tbl.model, conn.id = conn.id, verbose = FALSE)
     #output <- db.data.frame(tbl.output, conn.id = conn.id, verbose = FALSE)
 
-    #.restore.warnings(warnings)
-
     #Goal: output an object of class "lda.madlib" which has attributes:
     #assignments: a list length D of topic assignments for each word (in output table)
     #topics: a K x V matrix where a_ij is number of times word j is assigned to topic i
     #topic_sums: length K vector, topic distribution for corpus 
     #document_sums: K x D matrix, breaking down topic distribution into documents
-
-
-
-
-
-
-
-
-
-
 }
